05352a150fe984047231e0d026c4ab38c88f13bb
OGadmin <123mmitchell@gmail.com>
2022-07-16 19:16:44 -0400

    ArrayList implemented with getBook and addBook methods. Testing of ArrayList to follow.

--- src/Book.java
+++ src/Book.java
@@ -1,16 +1,50 @@
 import java.util.Date;
 import java.util.*;
-public class Book {
-    String title = null;
-    String author = null;
-    String description = null;
-    int totalPages = 0;
-    Date published;
-    List<Book> books = new ArrayList<Book>();
+import java.lang.*;
 
+public class Book {
+    String title;
+    String author;
+    String description;
+    int totalPages;
+    Date published;
+    List<Book> books = new ArrayList<Book>();;
+
+
+    public Book(String title, String author, String description, int totalPages, Book books){
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.totalPages = totalPages;
+        published = new Date();
+    }
     public void searchPageNum(int pageNum){} //find page number
     public void searchWord(String word){} //find word on page
     public String searchDefinition(String word){String definition =""; return definition; } //find word definition using web scrape class
     public String searchRoot(String word){String root =""; return root; } //find root word using web scrape class
-    //public Book getBook(String title, String author, String description, int totalPages){return books[title, author, description, totalPages];} //
+
+    /**
+     * Search for book by title or author in the array list of books.
+     **/
+    public Book getBook(String title, String author){
+        for(Book book: books) {
+            if(book.title.equals(title)) {
+                return (book);
+            }else if(book.author.equals(author)){
+                return (book);
+            }
+        }
+        return null;
+    }
+    public void addBook(String title, String author, String description, int totalPages, Book books){
+        if (title != null && author != null && description != null) {
+            books.add(title);
+            books.add(author);
+            books.add(description);
+        }
+    }
+
+    private void add(String title) {
+        this.title= title;
+    }
 }
