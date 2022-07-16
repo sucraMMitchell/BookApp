95580cb1fd042b5886fb8f6b317a9f3fd8dd6df2
OGadmin <123mmitchell@gmail.com>
2022-07-16 16:48:43 -0400

    Initial commit

---
+++ src/Shelf.java
@@ -0,0 +1,11 @@
+/**
+ * Store multiple books by type (genre, short/long read, reviews, etc)
+ *
+ *
+ **/
+
+class Shelf extends Book{
+    public Shelf(){}
+    public Shelf(int genre){}
+    public Shelf(int genre, String reviews) {}
+}
---
+++ src/WebScrape.java
@@ -0,0 +1,11 @@
+/**
+ * Pull book text from online using Socket, BufferedReader, and BufferedWriter classes.
+ * Stores the book text in book class fields.
+ *
+ *
+ *
+ **/
+
+class Webscrape extends Book{
+
+}
