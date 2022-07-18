15c2ce025e7d9c089086e702db3ed8dd28c91f3a
OGadmin <123mmitchell@gmail.com>
2022-07-18 19:51:06 -0400

    recommit

--- src/Shelf.java
+++ src/Shelf.java
@@ -3,9 +3,20 @@
  *
  *
  **/
+import java.util.Collections;
+import java.util.HashMap;
 
-class Shelf extends Book{
-    public Shelf(){}
-    public Shelf(int genre){}
-    public Shelf(int genre, String reviews) {}
+public class Shelf extends Book{
+
+    /**
+     * Search for book by title in the hash map of books.
+     * Key = ISBN number Value = Title, Author
+     **/
+    private Map<Integer, String> books = new HashMap<String, String>();
+
+    public Shelf(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
 }
